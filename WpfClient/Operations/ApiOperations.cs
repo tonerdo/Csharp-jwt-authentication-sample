5572ed8d16c403f72a854a29e8f92cfbd6319885
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-05-21 22:49:44 +0100

    add login functionality

---
+++ WpfClient/Operations/ApiOperations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using WpfClient.Models;
+using Newtonsoft.Json;
+
+namespace WpfClient.Operations
+{
+    class ApiOperations
+    {
+        private string baseUrl;
+        public ApiOperations()
+        {
+            this.baseUrl = "http://localhost:5000/api";
+        }
+
+        public User AuthenticateUser(string username, string password)
+        {
+            string endpoint = this.baseUrl + "/users/login";
+            string method = "POST";
+            string json = JsonConvert.SerializeObject(new
+            {
+                username = username,
+                password = password
+            });
+
+            WebClient wc = new WebClient();
+            wc.Headers["Content-Type"] = "application/json";
+            try
+            {
+                string response = wc.UploadString(endpoint, method, json);
+                return JsonConvert.DeserializeObject<User>(response);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
