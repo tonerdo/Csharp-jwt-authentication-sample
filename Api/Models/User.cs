9b339fb3aa6f1a5e49503df5c978ba64d9055093
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-05-21 16:15:01 +0100

    Add api db context and datastore to mimick entity framework

--- Api/Models/User.cs
+++ Api/Models/User.cs
@@ -13,7 +13,7 @@ namespace Api.Models
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
-        public string Filename { get; set; }
+        public string Firstname { get; set; }
         public string Middlename { get; set; }
         public string Lastname { get; set; }
         public int Age { get; set; }
